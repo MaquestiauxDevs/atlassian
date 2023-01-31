40e782791931adae00dfe0cdca4608666862db17
Jean-Philippe M <maquejp@gmail.com>
2023-01-31 12:08:25 +0100

    Mode sh in the right folder

---
+++ scripts/add_internals_to_hosts.sh
@@ -0,0 +1,10 @@
+sudo echo "
+
+# For Docker Atlassian
+127.0.0.1 jira.internal
+127.0.0.1 confluence.internal
+127.0.0.1 bitbucket.internal
+127.0.0.1 bamboo.internal
+127.0.0.1 mail.internal
+
+" >> /etc/hosts
