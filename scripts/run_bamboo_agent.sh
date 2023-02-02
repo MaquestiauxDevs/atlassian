9ea0681f24558e7b7049a4d16b4577d475dc3cf7
Jean-Philippe M <maquejp@gmail.com>
2023-02-02 11:36:29 +0100

    Add the download of the Bamboo agent when not present in the folder

--- scripts/run_bamboo_agent.sh
+++ scripts/run_bamboo_agent.sh
@@ -1,1 +1,3 @@
+curl http://bamboo.internal/agentServer/agentInstaller/atlassian-bamboo-agent-installer-9.1.1.jar --output atlassian-bamboo-agent-installer-9.1.1.jar
+
 java -jar ./atlassian-bamboo-agent-installer-9.1.1.jar http://bamboo.internal/agentServer/
